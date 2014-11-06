fa566d1a1efcc5421a1c0a8105bf04b0db83faa1
jweecyX <jonathanwcy88@hotmail.com>
2014-11-06 17:39:42 +1100

    Added register and user list functions

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,58 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Home' });
 });
+
+
+
+router.get('/userlist', function(req, res) {
+        
+    var db = req.db;
+    var collection = db.get('users');
+    collection.find({}, {}, function(e, doc){
+        res.render('userlist', {
+            "userlist" : doc,
+            title: 'User List'
+                       
+        });
+    });
+});
+
+
+
+router.get('/register', function(req, res){
+    res.render('register', { title: 'Register' })   
+
+});
+
+
+
+
+router.post('/adduser', function(req, res) {
+    var fn = req.body.firstname;
+    var ln = req.body.lastname;
+    var em = req.body.email;
+    var al = req.body.alias;
+    
+    var db = req.db;
+    var collection = db.get('users');
+    
+    collection.insert({ 
+        "firstname": fn, 
+        "lastname": ln, 
+        "alias": al, 
+        "email": em 
+    }, function(e, doc){                     
+        if(e){
+            res.send("There is something wrong.");
+        }
+        else{
+            res.location('/userlist');
+            res.redirect('/userlist');
+        }
+    });    
+});
+
 
 module.exports = router;
